5ac85c25c7dc53793b8aa1f48d87562ddea8798f
Ratthapon Sananaur <poom8165@gmail.com>
2021-08-24 18:02:50 +0700

    log value

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ const app = express()
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
+  console.log(req)
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err)
       res.status(500).end()
     })
-  console.log(req)
+  // console.log(req)
 })
 app.use('/api/send-notify', (req, res) => {
   // console.log('req', req.body)
